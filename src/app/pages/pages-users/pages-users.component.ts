a4c089d77b4745d15a114d9e0f4a8eaaeb586f83
Rama Raju <ramaraju.ganapathi@quadrantresource.com>
2023-03-10 10:46:06 +0530

    Update User

--- src/app/pages/pages-users/pages-users.component.ts
+++ src/app/pages/pages-users/pages-users.component.ts
@@ -11,10 +11,10 @@ import { UserServiceService } from 'src/app/services/user-service.service';
 })
 export class PagesUsersComponent implements OnInit {
   //submitted:boolean = false;
-
+  errorMessage:string='';
   loading!: boolean;
   usersList!: Array<User>
-
+  updateUserData:any;
   newUserForm = new FormGroup({
     name: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required]),
@@ -32,17 +32,7 @@ export class PagesUsersComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.userService.getAllUsers('Quad').pipe().pipe(first()).subscribe((data: any) => {
-      console.log(data)
-      if (Array.isArray(data)) {
-        this.usersList = data
-      }
-      this.loading = false;
-    },
-      (error: string) => {
-        console.log(error);
-        this.loading = false;
-      });
+   this.getallUsers();
   }
 
   submit() {
@@ -59,8 +49,12 @@ export class PagesUsersComponent implements OnInit {
       this.userService.addNewUser(payload,'Quad',emp_id).subscribe((result:any)=>{
         console.log("Result :", result)
         this.newUserForm.reset();
+        if(result.body="given data is already exist in dynamodb"){
+          this.errorMessage = "User Already Exist!"
+        }setTimeout(()=>this.errorMessage='',3000)
       })
     }
+    this.getallUsers();
   }
 
   get f() {
@@ -68,6 +62,39 @@ export class PagesUsersComponent implements OnInit {
   }
   formReset(){
     this.newUserForm.reset()
+  }
+  async updateUser(value: any){
+    // const payload:any = {
+    //   name:this.newUserForm.value.name,
+    //   email:this.newUserForm.value.email,
+    //   department: this.newUserForm.value.department,
+    //   designation:this.newUserForm.value.designation,
+    //   phone:this.newUserForm.value.phone
+    // }
+    // const emp_id:any = this.newUserForm.value.emp_id;
+    console.log("Value of Email: ",value)
+    await  this.userService.getUserbyEmail(value).subscribe((result:any)=>{
+      this.updateUserData = result[0];
+      console.log("this.updateUser",this.updateUserData)
+    },
+    (error:string)=>{
+      console.log("Error :",error)
+    }
+    )
+  }
+  
+  getallUsers(){
+    this.userService.getAllUsers('Quad').pipe().pipe(first()).subscribe((data: any) => {
+      console.log(data)
+      if (Array.isArray(data)) {
+        this.usersList = data
+      }
+      this.loading = false;
+    },
+      (error: string) => {
+        console.log(error);
+        this.loading = false;
+      });
   }
 
 }
